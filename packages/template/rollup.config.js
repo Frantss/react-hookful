3a779ef1eb1305c6e6a0639c2ea10885453f7d11
frantss <frantss11@gmail.com>
2020-11-14 15:58:12 -0300

    feat(all): add babel to compile to es5

--- packages/template/rollup.config.js
+++ packages/template/rollup.config.js
@@ -1,16 +1,35 @@
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 
 export default {
   input: 'src/index.ts',
   output: [
-    { file: 'dist/index.cjs.js', format: 'cjs', sourcemap: true },
-    { file: 'dist/index.esm.js', format: 'esm', sourcemap: true },
-    { file: 'dist/index.umd.js', name: '@react-hookful/core', format: 'umd', sourcemap: true },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.umd.js',
+      name: '@react-hookful/template',
+      format: 'umd',
+      sourcemap: true,
+    },
   ],
+  external: ['react'],
   plugins: [
     typescript({
       tsconfig: './tsconfig.json',
+    }),
+    getBabelOutputPlugin({
+      presets: ['@babel/preset-env'],
+      allowAllFormats: true,
     }),
     terser(),
   ],
